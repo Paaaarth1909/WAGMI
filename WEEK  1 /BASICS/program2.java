e7e7c8eaf799870a218c291402b8a0e6752871cf
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-25 22:15:33 +0530

    COMMIT

--- WEEK  1 /BASICS/program2.java
+++ WEEK  1 /BASICS/program2.java
@@ -1,17 +1,9 @@
 // Take name as input and print a greeting message.
-
-import java.util.Scanner;
-
 public class program2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.println("Hello, " + name + "! Welcome to Java programming.");
-
-        sc.close();
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World");
     }
 }
-
