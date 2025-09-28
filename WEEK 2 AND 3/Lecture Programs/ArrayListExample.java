786a3b9e76e21c3c6d55497929f9994ff1426611
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-28 18:40:38 +0530

    COMMIT

--- WEEK 2 AND 3/Lecture Programs/ArrayListExample.java
+++ WEEK 2 AND 3/Lecture Programs/ArrayListExample.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class ArrayListExample {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
