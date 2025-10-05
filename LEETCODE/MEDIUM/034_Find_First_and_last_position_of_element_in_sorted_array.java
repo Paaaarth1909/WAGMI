010acb05e98fabb781e921984d9890e5d379920d
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-05 12:36:05 +0530

    COMMIT

---
+++ LEETCODE/MEDIUM/034_Find_First_and_last_position_of_element_in_sorted_array.java
@@ -0,0 +1,60 @@
+/* Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+ */
+package LEETCODE.MEDIUM;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] a, int t) {
+        int l = 0, r = a.length - 1, ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (a[m] >= t) r = m - 1;
+            else l = m + 1;
+            if (a[m] == t) ans = m;
+        }
+        return ans;
+    }
+
+    private int findLast(int[] a, int t) {
+        int l = 0, r = a.length - 1, ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (a[m] <= t) l = m + 1;
+            else r = m - 1;
+            if (a[m] == t) ans = m;
+        }
+        return ans;
+    }
+}
