f6546e7f7ac57db0b056c3dab152fcf20d358aea
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-06 22:38:03 +0530

    COMMIT

--- LEETCODE/MEDIUM/033_Search_in_rotated_array_1.java
+++ LEETCODE/MEDIUM/033_Search_in_rotated_array_1.java
@@ -33,7 +33,7 @@ nums is an ascending array that is possibly rotated.
 
 
  
-public class Solution {
+class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
 
