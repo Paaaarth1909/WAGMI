e9c35bccbe3682b584673a3d29dd5299c865e5c9
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-10 22:26:59 +0530

    COMMIT

--- LEETCODE/MEDIUM/162_Find_Peak_Element.java
+++ LEETCODE/MEDIUM/162_Find_Peak_Element.java
@@ -27,4 +27,30 @@ Constraints:
 nums[i] != nums[i + 1] for all valid i.
  */
 
- 
+  class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid - 1] < nums[mid]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] > nums[end]) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+}
