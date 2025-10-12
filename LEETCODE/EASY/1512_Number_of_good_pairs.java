876af4575990fc72cf67da0896d04b6b092e1b61
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-12 22:54:59 +0530

    COMMIT

--- LEETCODE/EASY/1512_Number_of_good_pairs.java
+++ LEETCODE/EASY/1512_Number_of_good_pairs.java
@@ -26,3 +26,15 @@ Constraints:
 1 <= nums[i] <= 100
 */
 
+import java.util.*;
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int x : nums) {
+            count += map.getOrDefault(x, 0);
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        return count;
+    }
+}
