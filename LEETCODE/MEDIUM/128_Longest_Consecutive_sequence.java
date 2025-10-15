9c38e849b82475e40cf16a0d3c1dc2a1ecd94bf6
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-15 22:36:27 +0530

    COMMIT

--- LEETCODE/MEDIUM/128_Longest_Consecutive_sequence.java
+++ LEETCODE/MEDIUM/128_Longest_Consecutive_sequence.java
@@ -24,4 +24,25 @@ Constraints:
 0 <= nums.length <= 105
 -109 <= nums[i] <= 109
 */
- 
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) { 
+                int curr = num;
+                int length = 1;
+                while (set.contains(curr + 1)) {
+                    curr++;
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
