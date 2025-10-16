adcf9281d1967d19b793d6effe71b79c1e83029b
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-16 23:37:08 +0530

    COMMIT

--- LEETCODE/EASY/350_Intersection_of_2_arrays_2.java
+++ LEETCODE/EASY/350_Intersection_of_2_arrays_2.java
@@ -18,3 +18,26 @@ Constraints:
 1 <= nums1.length, nums2.length <= 1000
 0 <= nums1[i], nums2[i] <= 1000
 */
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for (int n : nums1)
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        
+        for (int n : nums2) {
+            if (map.containsKey(n) && map.get(n) > 0) {
+                result.add(n);
+                map.put(n, map.get(n) - 1);
+            }
+        }
+        
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++)
+            res[i] = result.get(i);
+        return res;
+    }
+}
