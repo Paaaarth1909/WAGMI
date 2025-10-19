f5935840304a0b13b952126eb6d9e3e9b9e29da1
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-19 23:57:27 +0530

    COMMIT

--- LEETCODE/EASY/206_Reverse_Linked_List.java
+++ LEETCODE/EASY/206_Reverse_Linked_List.java
@@ -1,3 +1,5 @@
+package LEETCODE.EASY;
+
 /* Given the head of a singly linked list, reverse the list, and return the reversed list.
 
  
@@ -22,4 +24,34 @@ Constraints:
 
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
