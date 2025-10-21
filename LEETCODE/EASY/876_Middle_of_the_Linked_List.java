9034ecbc19f83535dd6bd7986f87f37617b91828
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-21 16:11:07 +0530

    COMMIT

--- LEETCODE/EASY/876_Middle_of_the_Linked_List.java
+++ LEETCODE/EASY/876_Middle_of_the_Linked_List.java
@@ -23,4 +23,31 @@ Constraints:
 The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
 */
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
