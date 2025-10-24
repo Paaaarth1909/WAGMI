025e7cc39179aafebee68d5ea4072cd595ca2888
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-24 23:17:37 +0530

    COMMIT

--- LEETCODE/MEDIUM/ODD_Even_Linked_list_328.java
+++ LEETCODE/MEDIUM/ODD_Even_Linked_list_328.java
@@ -24,6 +24,34 @@ Constraints:
 
 The number of nodes in the linked list is in the range [0, 104].
 -106 <= Node.val <= 106
- 
-*/
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
 
