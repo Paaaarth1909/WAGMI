ac9c2591831346fdae59a1126322a68d62d4692a
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-29 23:52:08 +0530

    COMMIT

--- LEETCODE/MEDIUM/Sort_Colours_75.java
+++ LEETCODE/MEDIUM/Sort_Colours_75.java
@@ -22,3 +22,24 @@ n == nums.length
 1 <= n <= 300
 nums[i] is either 0, 1, or 2.
 */
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
