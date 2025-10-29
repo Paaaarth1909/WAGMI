0b0612b2668b2da1485bd820e57a7a9d6fea3953
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-29 23:53:57 +0530

    COMMIT

--- LEETCODE/EASY/Implement_Stack_using_Queues_225.java
+++ LEETCODE/EASY/Implement_Stack_using_Queues_225.java
@@ -35,3 +35,37 @@ Constraints:
 At most 100 calls will be made to push, pop, top, and empty.
 All the calls to pop and top are valid.
 */
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.offer(x);
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        return q1.poll();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
