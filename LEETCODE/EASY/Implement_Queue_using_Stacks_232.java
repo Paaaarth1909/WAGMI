0f2140cd806769c8bfdf0f8b5b7a78c3749f02db
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-30 23:43:42 +0530

    COMMIT

--- LEETCODE/EASY/Implement_Queue_using_Stacks_232.java
+++ LEETCODE/EASY/Implement_Queue_using_Stacks_232.java
@@ -36,3 +36,44 @@ At most 100 calls will be made to push, pop, peek, and empty.
 All the calls to pop and peek are valid.
 */
 
+import java.util.Stack;
+
+public class Implement_Queue_using_Stacks_232 {
+	public Stack<Integer> stack1;
+	public Stack<Integer> stack2;
+	public int size;
+	public Implement_Queue_using_Stacks_232() {
+		stack1 = new Stack<>();
+		stack2 = new Stack<>();
+		size = 0;
+	}
+
+	public void push(int x) {
+		stack1.push(x);
+		size++;
+	}
+
+	public int pop() {
+		if (stack2.isEmpty()) {
+			while (!stack1.isEmpty()) {
+				stack2.push(stack1.pop());
+			}
+		}
+		int value = stack2.pop();
+		size--;
+		return value;
+	}
+
+	public int peek() {
+		if (stack2.isEmpty()) {
+			while (!stack1.isEmpty()) {
+				stack2.push(stack1.pop());
+			}
+		}
+		return stack2.peek();
+	}
+
+	public boolean empty() {
+		return size == 0 ? true : false;
+	}
+}
