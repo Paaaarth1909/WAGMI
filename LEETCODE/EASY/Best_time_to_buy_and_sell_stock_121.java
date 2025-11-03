2de53f445ed563e45a8fa0add0d005444db0a260
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-03 20:19:51 +0530

    COMMIT

--- LEETCODE/EASY/Best_time_to_buy_and_sell_stock_121.java
+++ LEETCODE/EASY/Best_time_to_buy_and_sell_stock_121.java
@@ -1,3 +1,5 @@
+package LEETCODE.EASY;
+
 /* You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
@@ -24,3 +26,14 @@ Constraints:
 1 <= prices.length <= 105
 0 <= prices[i] <= 104
 */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) min = prices[i];
+            else profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
+}
