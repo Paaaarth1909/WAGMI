1c8d9e01b0bbd4493dabb343be5038640e95b437
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-13 21:18:10 +0530

    COMMIT

--- LEETCODE/EASY/Valid_Parentheses_20.java
+++ LEETCODE/EASY/Valid_Parentheses_20.java
@@ -44,3 +44,19 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 */
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
