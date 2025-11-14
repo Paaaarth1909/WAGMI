7a0f9ed50fcd208469c1000e6f674a102d4e7c7a
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-14 19:09:59 +0530

    COMMIT

--- LEETCODE/MEDIUM/Majority_Element_2_229.java
+++ LEETCODE/MEDIUM/Majority_Element_2_229.java
@@ -21,3 +21,39 @@ Constraints:
 1 <= nums.length <= 5 * 104
 -109 <= nums[i] <= 109
  */
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        Integer candidate1 = null, candidate2 = null;
+        int count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1) count1++;
+            else if (candidate2 != null && num == candidate2) count2++;
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        if (count1 > n / 3) res.add(candidate1);
+        if (count2 > n / 3) res.add(candidate2);
+        return res;
+    }
+}
