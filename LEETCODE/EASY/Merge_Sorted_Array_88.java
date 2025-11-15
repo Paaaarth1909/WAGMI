fb34dc5604f02937d2b28a72ead9084747b78379
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-15 23:22:30 +0530

    COMMIT

--- LEETCODE/EASY/Merge_Sorted_Array_88.java
+++ LEETCODE/EASY/Merge_Sorted_Array_88.java
@@ -35,3 +35,22 @@ nums2.length == n
 1 <= m + n <= 200
 -109 <= nums1[i], nums2[j] <= 109
 */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
